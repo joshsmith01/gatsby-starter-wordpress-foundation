d43bb63a8edee78a6a35362995f06f0bc3101ba7
Josh Smith <joshsmith01.contact@gmail.com>
2019-02-16 13:04:10 -0800

    Add Card component

---
+++ src/components/f/Card.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => (
+  <div className="card" style={{ width: '300px' }}>
+    <div className="card-divider">This is a header</div>
+    <img src="https://picsum.photos/200/300" alt="" />
+    <div className="card-section">
+      <h4>This is a card.</h4>
+      <p>
+        It has an easy to override visual style, and is appropriately subdued.
+      </p>
+    </div>
+  </div>
+)
+
+export default Card
